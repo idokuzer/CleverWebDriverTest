69c9393b5aa550b3e6ae36b65c2816f29992369a
idokuzer <ibrahimdokuzer@hotmail.com>
2022-12-06 23:13:11 +0300

    browserstack integration

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -116,7 +116,7 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: [
-        'appium'
+        'appium','browserstack'
     ],
     appium: {
         command: 'appium',
